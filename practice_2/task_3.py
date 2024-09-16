f6d0a3918f13061ad856e83cdce05a679ada77a4
Andrei <andrej_ilin7@mail.ru>
2024-09-17 01:14:40 +0300

    first commit

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
@@ -8,4 +7,8 @@ height = input()
 
 
 #code here
+def calculate_bmi(weight, height):
+    return weight / (height ** 2)
+
+print(calculate_bmi(weight=77, height=177))
 
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,23 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+def sum_of_digits(number):
+    digits = str(number)
+    total = 0
+    output = " + ".join(digits) + " = "
+    
+    for digit in digits:
+        total += int(digit)
+
+    return output, total
+while True:
+    user_input = input("Please enter a 4-digit integer: ")
+    
+    if user_input.isdigit() and len(user_input) == 4:
+        break
+    else:
+        print("Invalid input. Please make sure you enter a 4-digit integer.")
+
+output_string, total_sum = sum_of_digits(user_input)
+
+print(f"{output_string}{total_sum}")
